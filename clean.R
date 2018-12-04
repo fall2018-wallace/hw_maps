ea9565b01ebb6f969092f9f871984ecdc61ebe34
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 19:28:48 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -5,5 +5,13 @@ library(ggmap)
 dfStates <- raw_data
 cleandataframe <- function() #function declaration
 {
+dfStates <- dfStates[-1,]#removes the summary
+dfStates <- dfStates[-52,]#removes puerto rico
+dfStates
+dfStates <- dfStates[,-1]#removes the unwanted columns
+dfStates <- dfStates[,-1]#removes the unwanted columns
+dfStates <- dfStates[,-1]#removes the unwanted columns
+dfStates <- dfStates[,-1]#removes the unwanted columns
+colnames(dfStates)<- c("Statename", "population", "popOver18", "percentOver18")#renames the columns
   return(dfStates) #function body
 }
